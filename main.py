9bd05412787ba25cd1ddac9bb1e01b345fde935a
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-07-13 13:30:35 +0900

    first commit

--- main.py
+++ main.py
@@ -17,32 +17,49 @@ import models.dcgan as dcgan
 import models.mlp as mlp
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', required=True, help='cifar10 | lsun | imagenet | folder | lfw ')
+parser.add_argument('--dataset', required=True,
+                    help='cifar10 | lsun | imagenet | folder | lfw ')
 parser.add_argument('--dataroot', required=True, help='path to dataset')
-parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
-parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
-parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--workers', type=int,
+                    help='number of data loading workers', default=2)
+parser.add_argument('--batchSize', type=int,
+                    default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64,
+                    help='the height / width of the input image to network')
 parser.add_argument('--nc', type=int, default=3, help='input image channels')
-parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+parser.add_argument('--nz', type=int, default=100,
+                    help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
-parser.add_argument('--lrD', type=float, default=0.00005, help='learning rate for Critic, default=0.00005')
-parser.add_argument('--lrG', type=float, default=0.00005, help='learning rate for Generator, default=0.00005')
-parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
-parser.add_argument('--ngpu'  , type=int, default=1, help='number of GPUs to use')
-parser.add_argument('--netG', default='', help="path to netG (to continue training)")
-parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+parser.add_argument('--niter', type=int, default=25,
+                    help='number of epochs to train for')
+parser.add_argument('--lrD', type=float, default=0.00005,
+                    help='learning rate for Critic, default=0.00005')
+parser.add_argument('--lrG', type=float, default=0.00005,
+                    help='learning rate for Generator, default=0.00005')
+parser.add_argument('--beta1', type=float, default=0.5,
+                    help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--ngpu', type=int, default=1,
+                    help='number of GPUs to use')
+parser.add_argument('--netG', default='',
+                    help="path to netG (to continue training)")
+parser.add_argument('--netD', default='',
+                    help="path to netD (to continue training)")
 parser.add_argument('--clamp_lower', type=float, default=-0.01)
 parser.add_argument('--clamp_upper', type=float, default=0.01)
-parser.add_argument('--Diters', type=int, default=5, help='number of D iters per each G iter')
-parser.add_argument('--noBN', action='store_true', help='use batchnorm or not (only for DCGAN)')
+parser.add_argument('--Diters', type=int, default=5,
+                    help='number of D iters per each G iter')
+parser.add_argument('--noBN', action='store_true',
+                    help='use batchnorm or not (only for DCGAN)')
 parser.add_argument('--mlp_G', action='store_true', help='use MLP for G')
 parser.add_argument('--mlp_D', action='store_true', help='use MLP for D')
-parser.add_argument('--n_extra_layers', type=int, default=0, help='Number of extra layers on gen and disc')
-parser.add_argument('--experiment', default=None, help='Where to store samples and models')
-parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is rmsprop)')
+parser.add_argument('--n_extra_layers', type=int, default=0,
+                    help='Number of extra layers on gen and disc')
+parser.add_argument('--experiment', default=None,
+                    help='Where to store samples and models')
+parser.add_argument('--adam', action='store_true',
+                    help='Whether to use adam (default is rmsprop)')
 opt = parser.parse_args()
 print(opt)
 
@@ -50,7 +67,7 @@ if opt.experiment is None:
     opt.experiment = 'samples'
 os.system('mkdir {0}'.format(opt.experiment))
 
-opt.manualSeed = random.randint(1, 10000) # fix seed
+opt.manualSeed = random.randint(1, 10000)  # fix seed
 print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
@@ -67,24 +84,27 @@ if opt.dataset in ['imagenet', 'folder', 'lfw']:
                                    transforms.Scale(opt.imageSize),
                                    transforms.CenterCrop(opt.imageSize),
                                    transforms.ToTensor(),
-                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                   transforms.Normalize(
+                                       (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
 elif opt.dataset == 'lsun':
-    dataset = dset.LSUN(db_path=opt.dataroot, classes=['bedroom_train'],
+    dataset = dset.LSUN(root=opt.dataroot, classes=['bedroom_train'],
                         transform=transforms.Compose([
                             transforms.Scale(opt.imageSize),
                             transforms.CenterCrop(opt.imageSize),
                             transforms.ToTensor(),
-                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            transforms.Normalize(
+                                (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                         ]))
 elif opt.dataset == 'cifar10':
     dataset = dset.CIFAR10(root=opt.dataroot, download=True,
                            transform=transforms.Compose([
                                transforms.Scale(opt.imageSize),
                                transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               transforms.Normalize(
+                                   (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ])
-    )
+                           )
 assert dataset
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
                                          shuffle=True, num_workers=int(opt.workers))
@@ -97,6 +117,8 @@ nc = int(opt.nc)
 n_extra_layers = int(opt.n_extra_layers)
 
 # custom weights initialization called on netG and netD
+
+
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
@@ -104,6 +126,7 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
+
 
 if opt.noBN:
     netG = dcgan.DCGAN_G_nobn(opt.imageSize, nz, nc, ngf, ngpu, n_extra_layers)
@@ -113,7 +136,7 @@ else:
     netG = dcgan.DCGAN_G(opt.imageSize, nz, nc, ngf, ngpu, n_extra_layers)
 
 netG.apply(weights_init)
-if opt.netG != '': # load checkpoint if needed
+if opt.netG != '':  # load checkpoint if needed
     netG.load_state_dict(torch.load(opt.netG))
 print(netG)
 
@@ -142,11 +165,13 @@ if opt.cuda:
 
 # setup optimizer
 if opt.adam:
-    optimizerD = optim.Adam(netD.parameters(), lr=opt.lrD, betas=(opt.beta1, 0.999))
-    optimizerG = optim.Adam(netG.parameters(), lr=opt.lrG, betas=(opt.beta1, 0.999))
+    optimizerD = optim.Adam(
+        netD.parameters(), lr=opt.lrD, betas=(opt.beta1, 0.999))
+    optimizerG = optim.Adam(
+        netG.parameters(), lr=opt.lrG, betas=(opt.beta1, 0.999))
 else:
-    optimizerD = optim.RMSprop(netD.parameters(), lr = opt.lrD)
-    optimizerG = optim.RMSprop(netG.parameters(), lr = opt.lrG)
+    optimizerD = optim.RMSprop(netD.parameters(), lr=opt.lrD)
+    optimizerG = optim.RMSprop(netG.parameters(), lr=opt.lrG)
 
 gen_iterations = 0
 for epoch in range(opt.niter):
@@ -156,8 +181,8 @@ for epoch in range(opt.niter):
         ############################
         # (1) Update D network
         ###########################
-        for p in netD.parameters(): # reset requires_grad
-            p.requires_grad = True # they are set to False below in netG update
+        for p in netD.parameters():  # reset requires_grad
+            p.requires_grad = True  # they are set to False below in netG update
 
         # train the discriminator Diters times
         if gen_iterations < 25 or gen_iterations % 500 == 0:
@@ -190,7 +215,7 @@ for epoch in range(opt.niter):
 
             # train with fake
             noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
-            noisev = Variable(noise, volatile = True) # totally freeze netG
+            noisev = Variable(noise, volatile=True)  # totally freeze netG
             fake = Variable(netG(noisev).data)
             inputv = fake
             errD_fake = netD(inputv)
@@ -202,7 +227,7 @@ for epoch in range(opt.niter):
         # (2) Update G network
         ###########################
         for p in netD.parameters():
-            p.requires_grad = False # to avoid computation
+            p.requires_grad = False  # to avoid computation
         netG.zero_grad()
         # in case our last batch was the tail batch of the dataloader,
         # make sure we feed a full batch of noise
@@ -215,15 +240,19 @@ for epoch in range(opt.niter):
         gen_iterations += 1
 
         print('[%d/%d][%d/%d][%d] Loss_D: %f Loss_G: %f Loss_D_real: %f Loss_D_fake %f'
-            % (epoch, opt.niter, i, len(dataloader), gen_iterations,
-            errD.data[0], errG.data[0], errD_real.data[0], errD_fake.data[0]))
+              % (epoch, opt.niter, i, len(dataloader), gen_iterations,
+                 errD.data[0], errG.data[0], errD_real.data[0], errD_fake.data[0]))
         if gen_iterations % 500 == 0:
             real_cpu = real_cpu.mul(0.5).add(0.5)
-            vutils.save_image(real_cpu, '{0}/real_samples.png'.format(opt.experiment))
+            vutils.save_image(
+                real_cpu, '{0}/real_samples.png'.format(opt.experiment))
             fake = netG(Variable(fixed_noise, volatile=True))
             fake.data = fake.data.mul(0.5).add(0.5)
-            vutils.save_image(fake.data, '{0}/fake_samples_{1}.png'.format(opt.experiment, gen_iterations))
+            vutils.save_image(
+                fake.data, '{0}/fake_samples_{1}.png'.format(opt.experiment, gen_iterations))
 
     # do checkpointing
-    torch.save(netG.state_dict(), '{0}/netG_epoch_{1}.pth'.format(opt.experiment, epoch))
-    torch.save(netD.state_dict(), '{0}/netD_epoch_{1}.pth'.format(opt.experiment, epoch))
+    torch.save(netG.state_dict(),
+               '{0}/netG_epoch_{1}.pth'.format(opt.experiment, epoch))
+    torch.save(netD.state_dict(),
+               '{0}/netD_epoch_{1}.pth'.format(opt.experiment, epoch))
